75517e84f9ef66856573bcb15f5e4f492cd53edc
david-dacruz <dvddcrz@gmail.com>
2018-01-23 15:33:47 +0100

    fix 3rd loop to use with range

--- app.rb
+++ app.rb
@@ -1,1 +1,25 @@
-10.times { |i| puts "Hello world #{i + 1}" }
+# Boucle 1
+10.times { |i| puts "Hello world #{i + 1}"}
+
+# Boucle 2
+i = 0 
+while i < 10
+  puts "hello world"
+  i += 1
+end
+
+# Boucle 3
+do_this = [
+  "hello",
+  "hello",
+  "hello",
+  "hello",
+  "hello",
+  "hello",
+  "hello",
+  "hello",
+  "hello",
+  "hello"
+]
+
+do_this.each { |this| puts "#{this} world"}
